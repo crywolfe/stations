09cbf5ff75e3da49189d3c45007c888606e02f88
crywolfe <gwolfe2007@gmail.com>
2020-02-20 21:45:43 -0600

    add initial

---
+++ src/test/java/com/wolfe/stations/StationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolfe.stations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
