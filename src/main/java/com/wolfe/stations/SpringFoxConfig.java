1ac94006ebe1617430b8803ac2a7c58b218b6c57
crywolfe <gwolfe2007@gmail.com>
2020-02-26 20:25:04 -0600

    update swagger config.

--- src/main/java/com/wolfe/stations/SpringFoxConfig.java
+++ src/main/java/com/wolfe/stations/SpringFoxConfig.java
@@ -1,5 +1,6 @@
 package com.wolfe.stations;
 
+import io.swagger.annotations.Api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -15,7 +16,7 @@ public class SpringFoxConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
                 .paths(PathSelectors.any())
                 .build();
     }
