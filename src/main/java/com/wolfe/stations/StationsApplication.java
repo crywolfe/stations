f6a94dbbe12229bb7355b7b396a994038bc1ec10
crywolfe <gwolfe2007@gmail.com>
2020-02-28 23:48:10 -0600

    heroku "/" fix path

--- src/main/java/com/wolfe/stations/StationsApplication.java
+++ src/main/java/com/wolfe/stations/StationsApplication.java
@@ -3,9 +3,11 @@ package com.wolfe.stations;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableCaching
+@RequestMapping(value="/")
 public class StationsApplication {
 
     public static void main(String[] args) {
